1236dcee053684c682a9a692aad2283ce8ac95d8
oscar <oscarheel@unisabana.edu.co>
2019-01-20 19:41:37 -0500

    added interface and db connection

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Osqui',
-        'title': 'Post1',
-        'content': 'cmment1',
-        'date_posted': '10/10/2010'
-    },
-    {
-        'author': 'Osqui2',
-        'title': 'Post2',
-        'content': 'comment2',
-        'date_posted': '11/10/2010'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
